bf1831306c33f59cfb3b6ade2d2c6cbad29d3f03
BasLightyear <baskleisen@gmail.com>
2019-10-12 17:41:53 +0200

    Basic groundwork for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import AboutUs from './pages/AboutUs';
 import './css/styles.min.css';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">
+          <img className="App-logo" alt="logo" />
+        </Link>
+        <h1 className="App-title">Welcome to React</h1>
+        <Link to="/">Home</Link> <Link to="/post/">Post</Link>
       </header>
+      <Switch>
+        {/* <Route exact path="/" component={Home} />
+        <Route path="/post/" component={Post} /> */}
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
