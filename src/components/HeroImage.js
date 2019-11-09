8874d2e762cef903c4ad42527d017a535179a753
BasLightyear <baskleisen@gmail.com>
2019-11-09 13:19:10 +0100

    Fixed logo, sort of

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import logoWhite from '../img/logo - white.png';
+import { Container, Row } from 'react-bootstrap';
 
 const HeroImage = () => {
     return (
         <div className="hero-image-farm h-100 d-flex align-items-center">
             <div className="overlay overlay-jumbo"></div>
-            <Container fluid={true}>
-                <Row>
-                    <div className="col-sm-1 col-md-2 col-lg-1" />
-                    <div className="col-sm-5 col-md-8 col-lg-4">
-                        <img src={logoWhite} className="img-fluid" alt="Veldhuizen Perspak"/>
-                    </div>
-                    <div className="col-sm-6 col-md-2 col-lg-7" />
-                </Row>
-            </Container>
+            <img src={logoWhite} className="hero-logo hero-fade-in" alt="Veldhuizen Perspak"/>
         </div>
     )
 }
