7f5c6972cf29b7d358aacd24c903f55b68c38ff5
BasLightyear <baskleisen@gmail.com>
2019-11-17 15:27:43 +0100

    Fixed responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,47 +3,51 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import constants from '../Constants';
 import logo from '../img/logo - white.png'
+import backgroundFooter from '../img/floor_boards_cropped.jpg';
 
 const Footer = () => {
+    const backgroundStyle = {
+        backgroundImage: `url('${backgroundFooter}')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'bottom',
+        backgroundSize: 'cover'
+    }
+
     return (
-        <div className="footer">
+        <div className="footer" style={backgroundStyle}>
             <div className="overlay-footer" />
             <Container>
                 <Row>
-                    <div className="col-sm-5 col-md-4 col-xl-4">
+                    <div className="d-none d-md-block col-md-4 col-lg-3 mt-2">
                         <img
                             src={logo}
-                            // className="img-fluid"
+                            className="img-fluid"
                             alt="Veldhuizen Perspak"
                         />
                     </div>
-                    <div className="col-4">
-                        <Row>
-                            <h4 className="col-12 pl-0">Adres</h4>
-                            <div className="col-12 pl-0 pt-1">{constants.contact.adress}</div>
-                            <div className="col-12 pl-0 pt-1">{`${constants.contact.zipCode} ${constants.contact.city}`}</div>
-                        </Row>
+                    <div className="d-none d-lg-block col-lg-1"/>
+                    <div className="col-sm-12 col-md-4 mt-2">
+                            <h4>Adres</h4>
+                            <div className="pt-1">{constants.contact.adress}</div>
+                            <div className="pt-1">{`${constants.contact.zipCode} ${constants.contact.city}`}</div>
                     </div>
-                    <div className="col-4">
-                        <Row>
-                            <h3 className="col-12 pl-0">Contactgegevens</h3>
-                            <div className="col-11 pl-0 pt-1">
-                                <a href={`mailto:${constants.contact.email}`}>
-                                    <i className="fas fa-envelope mr-2"></i>
-                                    {constants.contact.email}
-                                </a>
-                            </div>
-                            <div className="col-11 pl-0 pt-1">
-                                <a href={`tel:${constants.contact.telephoneInternational}`}>
-                                    <i className="fas fa-phone-square-alt  mr-2"></i>
-                                    {constants.contact.telephoneDutch}
-                                </a>
-                            </div>
-                        </Row>
+                    <div className="col-sm-12 col-md-4 mt-2">
+                        <h4>Contactgegevens</h4>
+                        <div className="pt-1">
+                            <a href={`mailto:${constants.contact.email}`}>
+                                <i className="fas fa-envelope mr-2"></i>
+                                {constants.contact.email}
+                            </a>
+                        </div>
+                        <div className="pt-1">
+                            <a href={`tel:${constants.contact.telephoneInternational}`}>
+                                <i className="fas fa-phone-square-alt mr-2"></i>
+                                {constants.contact.telephoneDutch}
+                            </a>
+                        </div>
                     </div>
                 </Row>
             </Container>
-            
         </div>
     )
 }
